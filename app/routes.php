04655912568e2435206c6d4c6716dedd72a066bc
minako-ph <chanmina143@gmail.com>
2019-10-07 21:08:42 +0900

    update routes.php

--- app/routes.php
+++ app/routes.php
@@ -3,12 +3,15 @@
 $routes = [];
 
 $routes['/'] = function() {
+    ob_start();
     include __DIR__ . '/../app/view/index.phtml';
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
 $routes['/phpinfo.php'] = function () {
+    ob_start();
     phpinfo();
-    exit;
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
 return $routes;
--- public/index.php
+++ public/index.php
@@ -4,11 +4,16 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $routes = require __DIR__ . '/../app/routes.php';
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$not_found = function () {
+    return [404, ['Content-Type' => 'text/html'], "<h1>404 Not Found</h1>"];
+};
 
-if (isset($routes[$request_uri])) {
-    $f = $routes[$request_uri];
-    $f();
-} else {
-    http_response_code(404);
-    echo "<h1>404 Not Found</h1>";
+$f = $routes[$request_uri] ?? $not_found;
+[$status, $headers, $body] = $f();
+http_response_code($status);
+
+foreach ($headers as $name => $h){
+    header("{$name}: {$h}");
 }
+echo $body;
+
