441b709c65d66a6adf045c9fb0ef9f2db14f4d19
minako-ph <chanmina143@gmail.com>
2019-10-07 20:12:39 +0900

    update index.php

--- public/index.php
+++ public/index.php
@@ -2,5 +2,21 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-echo "<!DOCTYPE html>\n";
-echo "<p>素晴らしい山芋フレームワーク</p>\n";
+$routes = [];
+
+$routes['/'] = function() {
+    echo "<!DOCTYPE html>\n";
+    echo "<p>素晴らしい山芋フレームワークへようこそ</p>\n";
+};
+
+$routes['/phpinfo.php'] = function () {
+    phpinfo();
+};
+
+if (isset($routes[$_SERVER['REQUEST_URI']])) {
+    $f = $routes[$_SERVER['REQUEST_URI']];
+    $f();
+} else {
+    http_response_code( response_code(404));
+    echo "<h1>404 Not Found</h1>";
+}
